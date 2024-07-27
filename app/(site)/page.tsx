9166926bd3377f8eaaa185b8419dd33000d06245
Young-Gi Park <ghostyak@gmail.com>
2024-07-27 20:04:55 +0900

    loading 구현

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,8 @@
+import { sleep } from "@/lib/utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await sleep(2000);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
