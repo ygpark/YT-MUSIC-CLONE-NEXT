07c8c649c48fab5632835ac204f671b71ec8c465
Young-Gi Park <ghostyak@gmail.com>
2024-07-28 14:19:20 +0900

    ResponsiveText 컴포넌트

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,18 @@
 import { sleep } from "@/lib/utils";
 import Image from "next/image";
+import ResponsiveText from "@/components/elements/ResponsiveText";
 
 export default async function Home() {
-  return <div>Homepage</div>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ResponsiveText>Home</ResponsiveText>
+    </div>
+  );
 }
---
+++ components/elements/ResponsiveText.jsx
@@ -0,0 +1,17 @@
+/**
+ * 사용법:
+ *  <ResponsiveText>화면에 가득 채우고 싶은 단어</ResponsiveText>
+ */
+import React from "react";
+
+const ResponsiveText = ({ children }) => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="text-center" style={{ fontSize: "calc(100vw / 15)" }}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ResponsiveText;
